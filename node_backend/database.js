f368510885de4676600f50669316949c8fa6ae9c
Ajinkyap331 <ajinkyap331@gmail.com>
2022-10-21 16:14:52 +0530

    backend tweaks

--- node_backend/database.js
+++ node_backend/database.js
@@ -1,23 +1,10 @@
-const mongoose = require("mongoose");
 const express = require("express")
 const cors = require("cors")
 var bodyParser = require('body-parser');
 
-// models
-const EmployeeModel = mongoose.model('employee-model', new mongoose.Schema({
-    name: String,
-    username: String,
-    password: String,
-    email: String
-}))
-
-const ProductModel = mongoose.model('barcode-model', new mongoose.Schema({
-    name: String,
-    count: Number,
-    barcode: String,
-    inDate: String,
-    price: Number
-}))
+const { MongoClient } = require("mongodb");
+const uri = "mongodb+srv://admin:admin@cluster0.c7v5d8z.mongodb.net/?retryWrites=true&w=majority"
+const client = new MongoClient(uri);
 
 
 //express
@@ -27,51 +14,59 @@ app.use(bodyParser.json())
 app.use(cors())
 const port = process.env.PORT || 9000
 
-// connect mongo 
-mongoose.connect("mongodb://localhost:27017/new", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-// EmployeeModel.deleteMany({}, () => {
-//     console.log("deleted")
-// })
 
 
 // post [Employee]
-app.post('/employee', (req, res) => {
-    EmployeeModel.insertMany({
-        name: req.body.name,
-        username: req.body.username,
-        password: req.body.password
-    })
-    res.end("success")
-})
+// app.post('/employee', (req, res) => {
+//     EmployeeModel.insertMany({
+//         name: req.body.name,
+//         username: req.body.username,
+//         password: req.body.password
+//     })
+//     res.end("success")
+// })
 
-// get [EMPLOYEE]
-app.get('/employee', (req, res) => {
-    EmployeeModel.find((err, result) => {
-        res.send(result)
-    })
-})
+// // get [EMPLOYEE]
+// app.get('/employee', (req, res) => {
+//     EmployeeModel.find((err, result) => {
+//         res.send(result)
+//     })
+// })
+
+const database = client.db('database');
+
+async function run(req) {
+    try {
+        const products = database.collection("products")
+        const data = {
+            name: req.body.name,
+            count: req.body.count,
+            barcode: req.body.barcode,
+            inDate:  new Date().toLocaleString(),
+            price: req.body.price
+        }
+        const result = await products.insertOne(data)
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    }
+    finally {
+        await client.close()
+    }
+}
 
 // post [Products]
 app.post('/products', (req, res) => {
-    ProductModel.insertMany({
-        name: req.body.name,
-        count: req.body.count,
-        barcode: req.body.barcode,
-        inDate:  new Date().toLocaleString(),
-        price: req.body.price
-    })
+    run(req).catch(console.dir)
     res.end("success")
 })
 
 // get [Products]
 app.get('/products', (req, res) => {
-    ProductModel.find((err, result) => {
-        res.send(result)
-    })
+    const products = database.collection("products")
+    res.send(products.find({}))
+    // console.log(products)
+    // ProductModel.find((err, result) => {
+    //     res.send(result)
+    // })
 })
 
 app.listen(port, () => { console.log(`Listen on port ${port}`) })
